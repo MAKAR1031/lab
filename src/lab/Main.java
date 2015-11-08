e9698f7bfa3e9e0f7243f530ac09618188a420c5
Makarov Sergey <makar1488@gmail.com>
2015-11-08 15:40:08 +0300

    Second commit

--- src/lab/Main.java
+++ src/lab/Main.java
@@ -1,11 +1,15 @@
 package lab;
 
 public class Main {
-    
+
     private int sum(int a, int b) {
         return a + b;
     }
-    
+
+    private int sub(int a, int b) {
+        return a - b;
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello!");
     }
