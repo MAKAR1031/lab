003df92bc204cf6c2210642000b0649b8789f152
Makarov Sergey <makar1488@gmail.com>
2015-11-08 14:47:44 +0300

    First commit

---
+++ src/lab/Main.java
@@ -0,0 +1,12 @@
+package lab;
+
+public class Main {
+    
+    private int sum(int a, int b) {
+        return a + b;
+    }
+    
+    public static void main(String[] args) {
+        System.out.println("Hello!");
+    }
+}
